737e2b339e666a908d0ec3733ff16806b63754c2
flippedcoder <milecia.mcgregor@aol.com>
2022-07-04 18:54:25 -0500

    cleaned up repo

--- src/evaluate.py
+++ src/evaluate.py
@@ -13,7 +13,7 @@ if len(sys.argv) != 6:
     sys.exit(1)
 
 model_file = sys.argv[1]
-matrix_file = os.path.join(sys.argv[2], "test.pkl")
+test_file = os.path.join(sys.argv[2], "test.pkl")
 scores_file = sys.argv[3]
 prc_file = sys.argv[4]
 roc_file = sys.argv[5]
@@ -21,7 +21,7 @@ roc_file = sys.argv[5]
 with open(model_file, "rb") as fd:
     model = pickle.load(fd)
 
-with open(matrix_file, "rb") as fd:
+with open(test_file, "rb") as fd:
     matrix = pickle.load(fd)
 
 x = matrix.iloc[:,1:11].values
@@ -40,12 +40,13 @@ fpr, tpr, roc_thresholds = metrics.roc_curve(labels_pred, predictions, pos_label
 
 avg_prec = metrics.average_precision_score(labels_pred, predictions)
 roc_auc = metrics.roc_auc_score(labels_pred, predictions)
+    
+nth_point = math.ceil(len(prc_thresholds) / 1000)
+prc_points = list(zip(precision, recall, prc_thresholds))[::nth_point]
 
 with open(scores_file, "w") as fd:
     json.dump({"avg_prec": avg_prec, "roc_auc": roc_auc}, fd, indent=4)
     
-nth_point = math.ceil(len(prc_thresholds) / 1000)
-prc_points = list(zip(precision, recall, prc_thresholds))[::nth_point]
 with open(prc_file, "w") as fd:
     json.dump(
         {
--- src/prepare.py
+++ src/prepare.py
@@ -2,17 +2,9 @@ import pandas as pd
 from os.path import exists
 
 train_df = pd.read_csv("./data/train.csv")
-train_set1_df = pd.read_csv("./data/train_set1.csv")
-train_set2_df = pd.read_csv("./data/train_set2.csv")
 
 test_df = pd.read_csv("./data/test.csv")
-test_set1_df = pd.read_csv("./data/test_set1.csv")
-test_set2_df = pd.read_csv("./data/test_set2.csv")
 
 train_df.to_pickle('./data/train.pkl')
-train_set1_df.to_pickle('./data/train_set1.pkl')
-train_set2_df.to_pickle('./data/train_set2.pkl')
 
 test_df.to_pickle('./data/test.pkl')
-test_set1_df.to_pickle('./data/test_set1.pkl')
-test_set2_df.to_pickle('./data/test_set2.pkl')
